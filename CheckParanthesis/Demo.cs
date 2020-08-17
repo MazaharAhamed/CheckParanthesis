00f620bd4a6d79ef86eb61de63c65de9df341ea1
MazaharAhamed <mazaharahamed77@gmail.com>
2020-08-17 19:41:14 +0530

    Add project files.

---
+++ CheckParanthesis/Demo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace CheckParanthesis
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            String expression;
+
+            Console.Write("Enter an expression: ");
+            expression = Console.ReadLine();
+
+            if ( (IsValid(expression) )
+                Console.WriteLine("Valid Expression");
+            else
+                Console.WriteLine("Invalid Expression");
+
+            static bool IsValid(String expr)
+            {
+                StackA st = new StackA();
+                char ch;
+
+                for (int i = 0; i < expr.Length; i++)
+                {
+                    if (expr[i] == '(' || expr[i] == '{' || expr[i] == '[')
+                        st.Push(expr[i]);
+                    if (expr[i] == ')' || expr[i] == '}' || expr[i] == ']')
+                        if(st.isEmpty())
+                        {
+                            Console.WriteLine("Right Paranthesis are more than Left Paranthesis");
+                            return false;
+                        }
+                    else
+                        {
+                            ch = st.Pop();
+                            if (!MatchParantheses(ch, expr[i]))
+                            {
+                                Console.WriteLine("Mismatched Parantheses are: ");
+                                Console.WriteLine(ch + "and" + expr[i]);
+                                return false;
+                            }
+                        }
+                }
+                if (st.isEmpty())
+                {
+                    Console.WriteLine("Balanced Parantheses");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Left Parantheses are more than Right Parantheses");
+                    return false;
+                }
+            }
+            static bool MatchParantheses(char leftPar,char rightPar)
+            {
+                if (leftPar == '(' && rightPar == ')')
+                    return true;
+                if (leftPar == '{' && rightPar == '}')
+                    return true;
+                if (leftPar == '[' && rightPar == ']')
+                    return true;
+                return false;
+            }
+        }
+    }
+}
---
+++ CheckParanthesis/StackA.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckParanthesis
+{
+    class StackA
+    {
+        private char[] stackArray;
+        private int top;
+
+        public StackA()
+        {
+            stackArray = new char[10];
+            top = -1;
+        }
+
+        public StackA(int maxSize)
+        {
+            stackArray = new char[maxSize];
+            top = -1;
+        }
+
+        public bool isEmpty()
+        {
+            return (top == -1);
+        }
+
+        public bool isFull()
+        {
+            return (top == stackArray.Length - 1);
+        }
+
+        public void Push(char x)
+        {
+            if(isFull())
+            {
+                Console.WriteLine("Stack Overflow");
+                return;
+            }
+            top = top + 1;
+            stackArray[top] = x;
+        }
+
+        public char Pop()
+        {
+            char x;
+            if (isEmpty())
+            {
+                Console.WriteLine("Stack Underflow");
+            }
+            x = stackArray[top];
+            top = top - 1;
+            return x;
+        }
+    }
+}
